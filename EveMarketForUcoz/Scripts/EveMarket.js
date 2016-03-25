5c85fad12e503c90d45a3d658275c7593a84824f
Maksimov_A <maximovmsu@yandex.ru>
2016-03-26 00:55:27 +0300

    Первая версия

---
+++ EveMarketForUcoz/Scripts/EveMarket.js
@@ -0,0 +1,161 @@
+﻿$(document).ready(function () {
+    function moneyFormat(val, thSep, dcSep) {
+        return numberFormat(val.toFixed(2), thSep, dcSep);
+    }
+
+    function numberFormat(val, thSep, dcSep) {
+        // Проверка указания разделителя разрядов
+        if (!thSep) thSep = ' ';
+
+        // Проверка указания десятичного разделителя
+        if (!dcSep) dcSep = '.';
+
+        var res = val.toString();
+        var lZero = (val < 0); // Признак отрицательного числа
+
+        // Определение длины форматируемой части
+        var fLen = res.lastIndexOf('.'); // До десятичной точки
+        fLen = (fLen > -1) ? fLen : res.length;
+
+        // Выделение временного буфера
+        var tmpRes = res.substring(fLen);
+        var cnt = -1;
+        for (var ind = fLen; ind > 0; ind--) {
+            // Формируем временный буфер
+            cnt++;
+            if (((cnt % 3) === 0) && (ind !== fLen) && (!lZero || (ind > 1))) {
+                tmpRes = thSep + tmpRes;
+            }
+            tmpRes = res.charAt(ind - 1) + tmpRes;
+        }
+
+        return tmpRes.replace('.', dcSep);
+    }
+
+    function buildMarketCatalogHtml(marketGroups) {
+        var result = '';
+
+        if (marketGroups) {
+            for (var i = 0; i < marketGroups.length; i++) {
+                var marketGroup = marketGroups[i];
+                var groupName = marketGroup[0];
+                var subGroups = marketGroup[1];
+                var types = marketGroup[2];
+                result += '<div class="' + (types == null ? '' : 'market-group-has-types ') + 'market-group market-group-collapsed"><div class="market-group-header"><span class="market-group-header-arrow"></span><span class="market-group-header-text" ' + (types == null ? '' : 'data-types="' + JSON.stringify(types) + '"') + '>' + groupName + '</span></div><div class="market-group-content">' + buildMarketCatalogHtml(subGroups) + buildMarketTypesHtml(types) + '</div></div>';
+            }
+        }
+
+        return result;
+    }
+
+    function buildMarketTypesHtml(marketTypes) {
+        var result = '';
+
+        if (marketTypes) {
+            for (var i = 0; i < marketTypes.length; i++) {
+                var marketType = marketTypes[i];
+                var typeId = marketType[0];
+                var typeName = marketType[1];
+                result += '<div class="market-type"><span class="market-type-text" data-type="' + typeId + '">' + typeName + '</span></div>';
+            }
+        }
+
+        return result;
+    }
+    
+    function getMarketStat(types, systemId) {
+        typesCache = types;
+
+        var url = 'https://api.eve-central.com/api/marketstat/json?typeid=' + types.map(function (type) { return type[0]; }).toString();
+
+        if (systemId) {
+            url += '&usesystem=' + systemId;
+        }
+
+        var timer = null;
+
+        $.ajax({
+            dataType: "json",
+            url: url,
+            beforeSend: function (data) {
+                timer = setTimeout(function () {
+                    var MARKETDETAILS = $('#MARKETDETAILS');
+                    MARKETDETAILS.html('<img src="/Content/wait.gif"></img>');
+                }, 500);
+
+            },
+            success: function (data) {
+                clearTimeout(timer);
+                var MARKETDETAILS = $('#MARKETDETAILS');
+                MARKETDETAILS.html('');
+                for (var i = 0; i < data.length; i++) {
+                    var typeStat = data[i];
+                    typeStat['typeInfo'] = types.filter(function (type) { return type[0] == typeStat['buy']['forQuery']['types'][0] })[0];
+
+                    MARKETDETAILS.append(
+                        '<div><table><tr><td><img src="https://image.eveonline.com/Type/' + typeStat['typeInfo'][0] + '_64.png" /></td><td>' + typeStat['typeInfo'][1] + '</td></tr></table>'
+                        + '<table class="market-stat"><tr><td></td><th>Sell</th><th>Buy</th></tr>'
+                        + '<tr>'
+                        + '<tr><th>Volume</th>'
+                        + '<td>' + numberFormat(typeStat['sell']['volume']) + '</td><td>' + numberFormat(typeStat['buy']['volume']) + '</td>'
+                        + '</tr>'
+                        + '<tr><th>Min</th>'
+                        + '<td style="background-color: lightgreen">' + moneyFormat(typeStat['sell']['min']) + '</td><td>' + moneyFormat(typeStat['buy']['min']) + '</td>'
+                        + '</tr>'
+                        + '<tr><th>Max</th>'
+                        + '<td>' + moneyFormat(typeStat['sell']['max']) + '</td><td style="background-color: lightgreen">' + moneyFormat(typeStat['buy']['max']) + '</td>'
+                        + '</tr>'
+                        + '<tr><th>Avg</th>'
+                        + '<td>' + moneyFormat(typeStat['sell']['wavg']) + '</td><td>' + moneyFormat(typeStat['buy']['wavg']) + '</td>'
+                        + '</tr>'
+                        + '<tr><th>Median</th>'
+                        + '<td>' + moneyFormat(typeStat['sell']['median']) + '</td><td>' + moneyFormat(typeStat['buy']['median']) + '</td>'
+                        + '</tr>'
+                        + '<tr><th>5%</th>'
+                        + '<td>' + moneyFormat(typeStat['sell']['fivePercent']) + '</td>' + '<td>' + moneyFormat(typeStat['buy']['fivePercent']) + '</td>'
+                        + '</tr>'
+                        + '</tr></table></div><br><br>');
+                }
+            }
+
+        });
+    }
+
+    var typesCache = [];
+
+    var MARKETCATALOG = $("#MARKETCATALOG");
+    MARKETCATALOG.html(buildMarketCatalogHtml(marketCatalog));
+
+    $(".market-group-header-arrow, .market-group-header-text").click(function () {
+        var marketGroup = $(this).parent().parent();
+
+        if (marketGroup.hasClass('market-group-collapsed')) {
+            marketGroup.removeClass('market-group-collapsed');
+        }
+        else {
+            marketGroup.addClass('market-group-collapsed');
+        }
+    });
+
+    $(".market-type-text").click(function () {
+        var typeId = this.getAttribute('data-type');
+        var name = this.innerHTML;
+        var types = [[typeId, name]];
+
+        getMarketStat(types);
+    });
+
+    $(".market-group-has-types .market-group-header-text").click(function () {
+        var types = $(".market-type-text", $(this).parent().parent()).toArray().map(function (span) { return [span.getAttribute('data-type'), span.innerHTML] });
+
+        getMarketStat(types);
+    });
+
+    $('#SYSTEM').change(function () {
+        if (typesCache.length > 0) {
+            var systemId = this.value;
+
+            getMarketStat(typesCache, systemId);
+        }
+    });
+});
---
+++ MakeJsonData/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace MakeJsonData
+{
+    class MarketType
+    {
+        public int TypeID;
+        public string Name;
+    }
+
+    class MarketGroup
+    {
+        public string Name;
+        public MarketGroup[] SubGroups;
+        public MarketType[] MarketTypes;
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string inputFolder = @"E:\Backup\Eve-Market\Vanguard_1.0_114986_db\";
+            string outpuFolder = @"F:\Projects\EveMarket\EveMarketForUcoz\Scripts\";
+            string connectionString = ConfigurationManager.ConnectionStrings["Vanguard"].ConnectionString;//"Data Source = localhost; Initial Catalog = ebs_DATADUMP; Integrated Security = True";
+            string query =
+                @"
+                SELECT
+	                mg.marketGroupID, 
+	                mg.parentGroupID, 
+	                mg.marketGroupName, 
+	                mg.hasTypes
+	                FROM invMarketGroups AS mg
+	                ORDER BY 
+		                mg.parentGroupID, 
+		                mg.marketGroupName
+                ";
+
+
+            Dictionary<int, Dictionary<string, object>> yamlObject;
+            using (TextReader file = new StreamReader(inputFolder + "typeIDs.yaml"))
+            {
+                var deserializer = new Deserializer();
+                yamlObject = deserializer
+                    .Deserialize<Dictionary<int, Dictionary<string, object>>>(file);
+
+            }
+
+            //var marketYamlObject = yamlObject
+            //    .Where(item => item.Value.Keys.Contains("marketGroupID"))
+            //    .ToArray();
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(query, connectionString);
+            sda.Fill(dt);
+
+            MarketGroup[] MarketGroups = GetMarketGroups(dt, yamlObject, null);
+            string MarketCatalogJs = "var marketCatalog=" + GetMarketGroupsJson(MarketGroups) + ";";
+
+            File.WriteAllText(outpuFolder + "MarketCatalogData.js", MarketCatalogJs);
+        }
+
+        static string GetMarketGroupsJson(MarketGroup[] marketGroups)
+        {
+            string result = string.Join(",", marketGroups.Select(group => "[\"" + group.Name.Replace("\"", "\\\"") + "\"," + ((group.SubGroups == null) ? "" : GetMarketGroupsJson(group.SubGroups)) + ((group.MarketTypes == null) ? "" : "," + GetMarketTypesJson(group.MarketTypes)) + "]"));
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                result = "[" + result + "]";
+            }
+
+            return result;
+        }
+
+        static string GetMarketTypesJson(MarketType[] marketTypes)
+        {
+            string result = string.Join(",", marketTypes.OrderBy(marketType => marketType.Name).Select(marketType => "[" + marketType.TypeID + ",\"" + marketType.Name.Replace("\"", "\\\"") + "\"]"));
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                result = "[" + result + "]";
+            }
+
+            return result;
+        }
+
+        static MarketGroup[] GetMarketGroups(DataTable dt, Dictionary<int, Dictionary<string, object>> yamlObject, int? parentGroupID)
+        {
+            return dt.AsEnumerable()
+                .Where(row => row.Field<int?>("parentGroupID") == parentGroupID)
+                .Select(row => new MarketGroup
+                    {
+                        Name = row.Field<string>("marketGroupName"),
+                        SubGroups = row.Field<bool>("hasTypes") ? null : GetMarketGroups(dt, yamlObject, row.Field<int>("marketGroupID")),
+                        MarketTypes = row.Field<bool>("hasTypes") ? GetMarketTypes(yamlObject, row.Field<int>("marketGroupID")) : null
+                    }
+                )
+                .ToArray();
+        }
+
+        static MarketType[] GetMarketTypes(Dictionary<int, Dictionary<string, object>> yamlObject, int marketGroupID)
+        {
+            return yamlObject
+                .Where(item => item.Value.Keys.Contains("marketGroupID") && (string)item.Value["marketGroupID"] == marketGroupID.ToString())
+                .Select(item => new MarketType { TypeID = item.Key, Name = ((Dictionary<object, object>)item.Value["name"])["en"].ToString() })
+                .ToArray();
+        }
+    }
+}
